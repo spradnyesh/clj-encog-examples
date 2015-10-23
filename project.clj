821d4db34b733ab92b06baf79bf301bcd5b9cb8d
Pradnyesh Sawant <spradnyesh@gmail.com>
2015-10-23 09:15:17 +0530

    fix: 0.1.0: neural/lunar-lander

--- project.clj
+++ project.clj
@@ -1,6 +1,9 @@
-(defproject clj-encog-examples "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject clj-encog-examples "0.1.0"
+  :description "encog-examples rewritten in clojure"
+  :url "https://github.com/spradnyesh/clj-encog-examples"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.encog/encog-core "3.3.0"]]
+  :profiles {:repl {:dependencies [[org.clojure/tools.nrepl "0.2.10"]]
+                    :plugins [[cider/cider-nrepl "0.10.0-SNAPSHOT" :exclusions [org.clojure/tools.nrepl]]]}})
