149beb732a1322637de0532a90fd10021b27174f
Pradnyesh Sawant <spradnyesh@gmail.com>
2015-10-23 08:39:35 +0530

    there was darkness in the begining

---
+++ test/clj_encog_examples/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-encog-examples.core-test
+  (:require [clojure.test :refer :all]
+            [clj-encog-examples.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
